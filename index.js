78a0376be76ddb0bbcc80ea755c06ce2954f4101
thiet003 <thietdong264@gmail.com>
2024-08-03 04:16:03 +0700

    Connect Postgres

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 const express = require('express');
 const app = express();
+
+const pool = require('./databases/connect_to_postgre');
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+app.get('/dishs', (req, res) => {
+    pool.query('SELECT * FROM dishs', (error, result) => {
+        if (error) {
+            throw error;
+        }
+        res.send(result.rows);
+    });
+});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
